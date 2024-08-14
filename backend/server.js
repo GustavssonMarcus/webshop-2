1a479d2604c0f1fc259031b9f3db75841df62034
Marcus Gustavsson <mackan1892@outlook.com>
2024-08-14 09:54:17 +0200

    Added Post & Delete endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,46 @@
-const express = require('express');
+import {config} from 'dotenv';
+import express from 'express';
+import { connection } from './db/dbconnection.js';
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', async (req, res) => {
+
+    const [result] = await connection.query('SELECT * FROM coop')
+
+    res.send(result)
+    //res.send('Hello World!');
+});
+
+app.post('/post', async (req, res) => {
+    const {name, price} = req.body;
+    const [result] = await connection.query('INSERT INTO coop (name, price) VALUES (?, ?)', [name, price]);
+
+    res.send({result});
+})
+
+app.put('/put', (req, res) =>{
+
+})
+
+app.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const [result] = await connection.query('DELETE FROM coop WHERE id = ?', [id]);
+
+        if (result.affectedRows > 0) {
+            res.send({ message: `Post with id ${id} deleted successfully.` });
+        } else {
+            res.status(404).send({ message: `Post with id ${id} not found.` });
+        }
+    } catch (error) {
+        res.status(500).send({ message: 'Error deleting the post', error });
+    }
 });
 
 app.listen(port, () => {
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-//header('Content-type: text/csv');
-
 $ch = curl_init();
 
 $url = "http://localhost:3000";
@@ -10,6 +8,42 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
-var_dump($response);
+$responeObject = json_decode($response);
+?>
 
-echo "Hello World Hello";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COOP</title>
+</head>
+<body>
+    <h1>Matbutik</h1>
+    <?php foreach ($responeObject as $products) {?>
+            <h3>Namn: <?php echo $products->name ?></h3>
+            <h3>Pris: <?php echo $products->price ?></h3>
+       <?php } ?>
+    
+    
+</body>
+</html>
+
+<?php
+// $params = ['name' => 'Banan', 'price' => 20];
+
+
+// $defaults = array(
+//     CURLOPT_URL => 'http://localhost:3000/post',
+//     CURLOPT_POST => true,
+//     CURLOPT_POSTFIELDS => http_build_query($params),
+//     CURLOPT_RETURNTRANSFER => true
+// );
+
+// $ch = curl_init();
+// curl_setopt_array($ch, $defaults);
+
+// $response = curl_exec($ch);
+// //var_dump($response); 
+
+// curl_close($ch);
