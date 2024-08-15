bb92880dae47a0901d11bd8ee2aa7f849b537ff5
Marcus Gustavsson <mackan1892@outlook.com>
2024-08-15 15:36:26 +0200

    Test vercel

--- backend/api/db/dbconnection.js
+++ backend/api/db/dbconnection.js
@@ -6,11 +6,9 @@ console.log(process.env.HOST);
 console.log(process.env.USER);
 console.log(process.env.PASSWORD);
 console.log(process.env.DATABASE);
-export const connection = {}
-
-// await mysql.createConnection({
-//     host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE
-// });
+export const connection = await mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
