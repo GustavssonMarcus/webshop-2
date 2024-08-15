a4ab75552e585ddb2682761a29005700a3b9c791
Marcus Gustavsson <mackan1892@outlook.com>
2024-08-15 15:32:40 +0200

    vercel

--- backend/api/db/dbconnection.js
+++ backend/api/db/dbconnection.js
@@ -2,10 +2,15 @@ import mysql from 'mysql2/promise';
 import {config} from 'dotenv';
 
 config()
+console.log(process.env.HOST);
+console.log(process.env.USER);
+console.log(process.env.PASSWORD);
+console.log(process.env.DATABASE);
+export const connection = {}
 
-export const connection = await mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
+// await mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// });
