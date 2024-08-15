d618fc413e827491d543ce5c4bd292de57b9d086
Marcus Gustavsson <mackan1892@outlook.com>
2024-08-15 14:37:43 +0200

    Fix

--- backend/api/db/dbconnection.js
+++ backend/api/db/dbconnection.js
@@ -3,9 +3,10 @@ import {config} from 'dotenv';
 
 config()
 
-export const connection = await mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
+export const connection = {} 
+// await mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// });
