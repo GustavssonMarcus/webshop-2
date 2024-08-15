3c35e091bf759820449a32e16e2f0c9984c090c8
Marcus Gustavsson <mackan1892@outlook.com>
2024-08-15 15:39:33 +0200

    fix vercel

--- backend/api/db/dbconnection.js
+++ backend/api/db/dbconnection.js
@@ -10,5 +10,6 @@ export const connection = await mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.PORT
 });
